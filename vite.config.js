4ebc9e910f27698ca7b1c92bff7837e600940a51
yeatasim-cse9 <yeatasim.cse9.bu@gmail.com>
2026-03-02 22:42:04 +0600

    fix: chunk size warning with code splitting

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,15 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          firebase: ['firebase/app', 'firebase/auth', 'firebase/firestore'],
+          pdf: ['html2pdf.js'],
+        }
+      }
+    }
+  }
 })
